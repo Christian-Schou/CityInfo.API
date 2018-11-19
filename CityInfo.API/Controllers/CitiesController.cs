e16de7f1a0a11bd404cab6f8acc04daac8a32fec
Christian Schou <christian.schou17@gmail.com>
2018-11-19 11:46:38 +0100

    Create CitiesController.cs
    Added cities controller and defined route

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet()]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            // find city
+            var cityToReturn = 
+                CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == id);
+
+            // If city is not found
+            // we will return a 404 Not found HTTP status code
+            if(cityToReturn == null)
+            {
+                return NotFound();
+            }
+
+            // Else we will return a 
+            // HTTP status code 200 with the specified city
+            return Ok(cityToReturn);
+        }
+    }
+}

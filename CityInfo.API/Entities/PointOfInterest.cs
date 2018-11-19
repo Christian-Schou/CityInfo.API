ce501fc90ef471d9460dc95aac96980596f7744b
Christian Schou <christian.schou17@gmail.com>
2018-11-19 11:44:26 +0100

    Create PointOfInterest.cs
    Added Database DTO for POIS and defined Primary Key + Foreign Key

---
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,29 @@
+﻿// Christian Schou
+// PointOfInterest.cs
+//
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities
+{
+    public class PointOfInterest
+    {
+        // Primary key in database
+        // Identity creation for "on add" - a new key will be generated, 
+        // when a point of interest is added
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        // Navigation property - the forerign key
+        // its a depended class. Also called a convetion based approach
+
+        [ForeignKey("CityId")]
+        public City city { get; set; }
+        public int CityId { get; set; }
+    }
+}

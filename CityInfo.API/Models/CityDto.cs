c0f6087ae7e588dfd616f9a28ba184f0786e51c5
Christian Schou <christian.schou17@gmail.com>
2018-11-19 11:43:35 +0100

    Create CityDto.cs
    Created City DTO and implemented functionality for calculating POIs in list

---
+++ CityInfo.API/Models/CityDto.cs
@@ -0,0 +1,31 @@
+﻿// Christian Schou
+// CityDto.cs
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models
+{
+    public class CityDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int NumberOfPointsOfInterest
+        {
+            get
+            {
+                return PointsOfInterest.Count;
+            }
+        }
+
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; }
+        = new List<PointOfInterestDto>();
+    }
+}

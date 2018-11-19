29ac7b360a5454768ebbc5a252009a3520e85213
Christian Schou <christian.schou17@gmail.com>
2018-11-19 11:40:05 +0100

    Create IMailService.cs
    Implemented a mail Service interface

---
+++ CityInfo.API/Services/IMailService.cs
@@ -0,0 +1,11 @@
+﻿// Christian Schou
+// IMailService.cs
+//
+using System;
+namespace CityInfo.API.Services
+{
+    public interface IMailService
+    {
+        void Send(string subject, string message);
+    }
+}

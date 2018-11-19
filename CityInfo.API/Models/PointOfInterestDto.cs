dc2eb5ce2ad7fc5ca91f0c4e8c6322dd7b121109
Christian Schou <christian.schou17@gmail.com>
2018-11-19 11:43:00 +0100

    Create PointOfInterestDto.cs
    Created POI DTO

---
+++ CityInfo.API/Models/PointOfInterestDto.cs
@@ -0,0 +1,19 @@
+﻿// Christian Schou
+// PointOfInterestDto.cs
+//
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CityInfo.API.Models
+{
+    public class PointOfInterestDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}

6906139e24211816451ade5bfdedc331451de5f8
Christian Schou <christian.schou17@gmail.com>
2018-11-19 11:42:48 +0100

    Create PointOfInterestForCreationDto.cs
    Added DTO for creating POIs incl. requirements

---
+++ CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,19 @@
+﻿// Christian Schou
+// PointOfInterestForCreationDto.cs
+//
+using System;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace CityInfo.API.Models
+{
+    public class PointOfInterestForCreationDto
+    {
+        [Required(ErrorMessage = "You should provide a name value.")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+    }
+}

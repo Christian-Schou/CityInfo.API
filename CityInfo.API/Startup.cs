4ba361d8609cf2cbb90ae2d340205360e31fc08a
Christian Schou <christian.schou17@gmail.com>
2018-11-19 11:39:10 +0100

    Create Startup.cs
    Created startup file with configuration and added MVC to the project

---
+++ CityInfo.API/Startup.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using CityInfo.API.Services;
+using Microsoft.Extensions.Configuration;
+using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API
+{
+    public class Startup
+    {
+
+        //public static IConfigurationRoot Configuration;
+
+        public static IConfiguration Configuration { get; private set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        //public Startup(IHostingEnvironment env)
+        //{
+        //    var builder = new ConfigurationBuilder()
+        //        .SetBasePath(env.ContentRootPath)
+        //        .AddJsonFile("appSettings.json", optional:false, reloadOnChange:true);
+
+        //    Configuration = builder.Build();
+        //}
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // On the MVC service, we can add Json options
+            services.AddMvc()
+                    .AddMvcOptions(o => o.OutputFormatters.Add(
+                        new XmlDataContractSerializerOutputFormatter()));
+            //.AddJsonOptions(o =>
+            //{
+            //if (o.SerializerSettings.ContractResolver != null)
+            //        {
+            //            var castedResolver = o.SerializerSettings.ContractResolver
+            //                                as DefaultContractResolver;
+
+            //            // From this moment JSON.NET will simply take the property names 
+            //            // as they are defined on our class
+            //            castedResolver.NamingStrategy = null;
+            //        }
+            //});
+
+#if DEBUG
+            services.AddTransient<IMailService, LocalMailServices>();
+#else
+            services.AddTransient<IMailService, CloudMailServices>();
+#endif
+            var connectionString = @"Server=(localdb)\mssqllocaldb;Database=CityInfoDB;Trusted_Connection=true;";
+            services.AddDbContext<CityInfoContext>(x => x.UseSqlServer(connectionString));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+
+            loggerFactory.AddNLog();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler();
+            }
+
+            app.UseStatusCodePages();
+
+            app.UseMvc();
+
+            //app.Run((context) =>
+            //{
+            //    throw new Exception("Example exception");
+            //});
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //}
+        }
+    }
+}

1417718edce757626ea9037cdaea324ac6aca3e1
Christian Schou <christian.schou17@gmail.com>
2018-11-19 11:44:57 +0100

    Create CityInfoContext.cs
    Added the Database Context file for the application

---
+++ CityInfo.API/Entities/CityInfoContext.cs
@@ -0,0 +1,27 @@
+﻿// Christian Schou
+// CityInfoContext.cs
+//
+using System;
+using Microsoft.EntityFrameworkCore;
+namespace CityInfo.API.Entities
+{
+    public class CityInfoContext : DbContext
+    {
+        public CityInfoContext(DbContextOptions<CityInfoContext> options)
+            : base(options)
+        {
+            //
+        }
+
+        public DbSet<City> Cities { get; set; }
+
+        public DbSet<PointOfInterest> PointsOfInterest { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("connectionsstring");
+
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
